2fe0c9031c56079113b9cb19c9b8bebe5dc7debe
suraj <suraj200981@yahoo.co.uk>
2023-01-16 19:34:12 +0100

    working on isVaild

---
+++ src/Easy/String/ValidParentheses.java
@@ -0,0 +1,41 @@
+package Easy.String;
+
+import java.util.HashMap;
+
+public class ValidParentheses {
+
+  public ValidParentheses() {
+
+  }
+
+  public boolean isValid(String s) {
+
+    HashMap<Character, Character> map = new HashMap<>();
+
+    map.put('(', ')');
+    map.put('{', '}');
+    map.put('[', ']');
+
+    boolean output = false;
+    for (int x = 0; x < s.length(); x++) {
+      if (x != 0 && !output) {
+        break;
+      }
+      for (int y = x + 1; y < s.length(); y++) {
+        try {
+          if (map.get(s.charAt(x)) == s.charAt(y)) {
+            output = true;
+            x++;
+            break;
+          } else {
+            output = false;
+          }
+        } catch (NullPointerException e) {
+          output = false;
+          break;
+        }
+      }
+    }
+    return output;
+  }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import Easy.*;
-import Easy.String.CellsInExcelSheet;
-import Easy.String.DefangingIPAddress;
-import Easy.String.GoalParserInterpretation;
-import Easy.String.JewelsAndStones;
+import Easy.String.*;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        CellsInExcelSheet program = new CellsInExcelSheet();
-        System.out.println("Output: "+ program.cellsInRange("K1:L2"));
+        ValidParentheses program = new ValidParentheses();
+        System.out.println("Output: "+ program.isValid(")(){}"));
 
     }
 }
