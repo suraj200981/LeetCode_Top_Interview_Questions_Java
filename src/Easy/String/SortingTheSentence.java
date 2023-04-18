eaeaafb31bc143463fb8b8d819f9400fccb1bc8b
suraj <suraj200981@yahoo.co.uk>
2023-04-18 19:19:53 +0100

    solved but i dont think the code is very efficient

--- src/Easy/String/SortingTheSentence.java
+++ src/Easy/String/SortingTheSentence.java
@@ -1,27 +1,61 @@
 package Easy.String;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class SortingTheSentence {
 
   public String sortSentence(String s) {
 
-    int countOfCurrentWord=0;
-    HashMap<String, String> wordsAndIndex = new HashMap<String, String>();
+    ArrayList<String> wordsInSentence = new ArrayList<>();
 
-    for (int x=0; x<s.length(); x++){
+    ArrayList<Integer> numbers = new ArrayList<>();
 
-      if(Character.isSpaceChar(s.charAt(x))){
+    int beginning = 0;
+
+    for (int x = 0; x < s.length(); x++) {
+
+      if (Character.isSpaceChar(s.charAt(x))) {
         x++;
-      }else {
+        beginning = x;
+      } else {
         if (Character.isDigit(s.charAt(x))) {
-            wordsAndIndex.put(s.);
+
+          numbers.add(Integer.valueOf(String.valueOf(s.charAt(x)))); //these numbers will be sorted
+          wordsInSentence.add(s.substring(beginning, x + 1));
         } else {
-          countOfCurrentWord++;
+
         }
       }
     }
+    Collections.sort(numbers);
 
-    return "";
+    int x = 0;
+    int y = 0;
+    StringBuilder stringBuilder = new StringBuilder();
+
+    while (true) {
+      if (y == numbers.size()) {
+        break;
+      }
+
+      if (wordsInSentence.get(x).contains(String.valueOf(numbers.get(y)))) {
+
+        if (y == numbers.size() - 1) {
+          stringBuilder.append(wordsInSentence.get(x).replaceAll("[0-9]", ""));
+          y++;
+          x = 0;
+        } else {
+          stringBuilder.append(wordsInSentence.get(x).replaceAll("[0-9]", "")).append(" ");
+          y++;
+          x = 0;
+        }
+      } else {
+        x++;
+      }
+    }
+
+    return stringBuilder.toString();
   }
 }
