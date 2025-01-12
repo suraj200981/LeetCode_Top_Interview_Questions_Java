bcfb050370706edc78da23471cbaa3899be6f4b6
suraj <suraj200981@yahoo.co.uk>
2025-01-12 11:18:20 +0000

    ReverseVowels solved in o(N) using two pointers technique

--- src/Easy/ReverseVowels.java
+++ src/Easy/ReverseVowels.java
@@ -1,5 +1,26 @@
 package Easy;
+/*345. Reverse Vowels of a String
+Solved
+Easy
+Topics
+Companies
+Given a string s, reverse only all the vowels in the string and return it.
 
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+
+
+Example 1:
+
+Input: s = "IceCreAm"
+
+Output: "AceCreIm"
+
+Explanation:
+
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+*/
 public class ReverseVowels {
   public String reverseVowels(String s) {
     int right = s.length() - 1;
